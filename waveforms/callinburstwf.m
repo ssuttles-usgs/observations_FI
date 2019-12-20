3976bc31c9f303b2be6968d43470a9688da4ee84
Steve Suttles <ssuttles@usgs.gov>
2019-12-20 11:14:15 -0500

    updates to inburst viewer

--- waveforms/callinburstwf.m
+++ waveforms/callinburstwf.m
@@ -26,16 +26,39 @@ clf
 scrsz=get(0,'screensize');
 set(gcf,'position',[50 50 scrsz(3)*0.8 scrsz(4)*0.8]);
 
-ax1=axes('position',[0.1 0.6 0.8 0.3]);
+ax0=axes('position',[0.1 0.7 0.8 0.2]);
+ax1=axes('position',[0.1 0.45 0.8 0.2]);
+ax2=axes('position',[0.1 0.1 0.2 0.3])
+ax3=axes('position',[0.4 0.1 0.2 0.3])
+ax4=axes('position',[0.7 0.1 0.2 0.3])
+
+
 tall=([wf.dn]-[wf(1).dn(1)])*86400;
 uball=[wf.ub];
 
-plot(tall,uball)
+len=length(wf);
+
+nwf=len
+
+
+for ii=1:len-nwf+1
+
+if exist('ax1')
+    cla(ax1,'reset')
+end
+if exist('ax2')
+    cla(ax2,'reset')
+end
+if exist('ax3')
+    cla(ax3,'reset')
+end
+ 
+axes(ax1)    
+plot(tall,uball,'b')
 hold on
-ax2=axes('position',[0.1 0.1 0.25 0.3])
-nwf=10
-swf=1
+swf=ii;
 for i=swf:swf+nwf-1
+axes(ax2)
 uhat=sqrt((2/wf(i).T).*trapz(wf(i).t,wf(i).ub.^2));
 plot(wf(i).t./wf(i).T,wf(i).ub./uhat)
 hold on
@@ -43,10 +66,11 @@ end
 line([0 1],[0 0],'color',[0.75 0.75 0.75])
 
 wfr=findwfr(wf(swf:swf+nwf-1));
-
+%tic
 wfr.wf7=findwf7(wfr);
+%toc
 
-ax3=axes('position',[0.4 0.1 0.25 0.3]) 
+axes(ax3)
 plot(0,0,'ok','MarkerFaceColor','r')
 hold on
 plot(wfr.Tcu,wfr.umax,'ok','MarkerFaceColor','r')
@@ -56,7 +80,17 @@ plot(wfr.T,0,'ok','MarkerFaceColor','r')
 line([0 wfr.T],[0 0],'color',[0.75 0.75 0.75])
 plot(wfr.wf7.t,wfr.wf7.ub,'-k')
 
+axes(ax1)
+iplt=swf:swf+nwf-1;
+tplt=([wf(iplt).dn]-[wf(1).dn(1)])*86400;
+ubplt=[wf(iplt).ub];
+plot(tplt,ubplt,'r')
 
+figure(1)
+pause(1)
+
+
+end
 %%
 % nseg=floor(length(p2)/nfft);
 %  m=fix(length(p2)/nseg);
